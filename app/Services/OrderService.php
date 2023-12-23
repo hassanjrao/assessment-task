b1eabe87fe98b3e82b3b7317d4b80fbad5b07420
hassanjrao <hassanjrao@gmail.com>
2023-12-24 02:25:14 +0400

    final touch 1.1

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -34,20 +34,34 @@ class OrderService
             return;
         }
 
+        // get merchant
         $merchant = Merchant::where('domain', $data['merchant_domain'])
         ->with(['affiliate'])
         ->first();
 
 
+        // create affiliate if not exists
         $this->affiliateService->register($merchant, $data['customer_email'], $data['customer_name'], $merchant->default_commission_rate);
 
 
+        // calculate commission
+        $commission = $data['subtotal_price'] * $merchant->affiliate->commission_rate;
+
+        
+        // log commission
+        Log::info('OrderService@processOrder:commission', [
+            'merchant_id' => $merchant->id,
+            'affiliate_id' => $merchant->affiliate->id,
+            'commission' => $commission
+        ]);
+
+        // create order
         Order::create(
             [
                 'merchant_id' => $merchant->id,
                 'affiliate_id' => $merchant->affiliate->id,
                 'subtotal' => $data['subtotal_price'],
-                'commission_owed' => $data['subtotal_price'] * $merchant->affiliate->commission_rate,
+                'commission_owed' => $commission,
                 'discount_code' => $data['discount_code'],
                 'external_order_id' => $data['order_id'],
             ]
